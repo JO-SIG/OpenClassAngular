0284165eb601da83cae3f183c43fe07f59d2dd43
JO-SIG <jo.sig@hotmail.com>
2019-02-14 22:33:15 +0100

    OpenClass Angular Cours : Améliorez la structure du code avec les Services

---
+++ src/app/services/appareil.service.ts
@@ -0,0 +1,57 @@
+export class AppareilService {
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'éteint'
+    },
+    {
+      name: 'Frigo',
+      status: 'allumé'
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint'
+    }
+  ];
+
+  switchOnAll() {
+    for(let appareil of this.appareils) {
+      appareil.status = 'allumé';
+    }
+  }
+
+  switchOffAll() {
+    for(let appareil of this.appareils) {
+      appareil.status = 'éteint';
+    }
+  }
+
+  onAllumer() {
+    this.switchOnAll();
+  }
+
+  onEteindre() {
+    if(confirm('Etes-vous sûr de vouloir éteindre tous vos appareils ?')) {
+      this.switchOffAll();
+    } else {
+      return null;
+    }
+  }
+
+  switchOnOne(i: number) {
+    this.appareils[i].status = 'allumé';
+  }
+
+  switchOffOne(i: number) {
+    this.appareils[i].status = 'éteint';
+  }
+
+  onSwitch(appareilStatus, index) {
+    console.log(appareilStatus);
+    if(appareilStatus === 'allumé') {
+      this.switchOffOne(index);
+    } else if(appareilStatus === 'éteint') {
+      this.switchOnOne(index);
+    }
+  }
+}
